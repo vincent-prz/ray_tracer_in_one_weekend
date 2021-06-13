5aa17b09d887d1e27f6c4119d0f0d5ab8dd1a713
Vincent Perez <perezv815@gmail.com>
2021-06-13 21:22:00 +0200

    6.1

--- src/ray.rs
+++ src/ray.rs
@@ -13,17 +13,24 @@ impl Ray {
     }
 }
 
-fn hit_sphere(center: &Point3, radius: f64, ray: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f64, ray: &Ray) -> f64 {
     let oc = ray.origin - *center;
     let a = dot(&ray.direction, &ray.direction);
     let b = 2.0 * dot(&ray.direction, &oc);
     let c = dot(&oc, &oc) - radius * radius;
-    b * b - 4.0 * a * c > 0.0
+    let discriminant = b * b - 4.0 * a * c;
+    if discriminant < 0.0 {
+        return -1.0;
+    }
+    (-b - discriminant.sqrt()) / (2.0 * a)
 }
 
 pub fn ray_color(ray: &Ray) -> color::Color {
-    if hit_sphere(&Vec3(0.0, 0.0, -1.0), 0.5, ray) {
-        return Vec3(1.0, 0.0, 0.0);
+    let t = hit_sphere(&Vec3(0.0, 0.0, -1.0), 0.5, ray);
+    if t > 0.0 {
+        let normal = unit_vector(&(ray.at(t) - Vec3(0.0, 0.0, -1.0)));
+        // 0.5(v + 1) -> [-1, 1] => [0, 1]
+        return 0.5 * Vec3(normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0);
     }
     let unit_direction = unit_vector(&ray.direction);
     let t = 0.5 * (unit_direction.y() + 1.0);
